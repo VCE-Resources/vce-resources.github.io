62cc66dba2c2017f57cbd1bc58389d99d118bf21
Mid <89073237+hydra-derp@users.noreply.github.com>
2023-05-07 10:54:31 +1000

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,66 @@
-console.log("Javascript is running");
+/*----------------------------------------------------*/
+/* Quote Loop
+------------------------------------------------------ */
 
-var dropdown = document.getElementsByClassName("drop-button");
-
-var i;
-
-for(i=0; i < dropdown.length; i++) {
-  console.log("Inside for loop");
-  dropdown[i].addEventListener("click", function() {
-    console.log("click eventlistener added");
-    this.classList.toggle("active");
-  var dropdownContent = this.nextElementSibling;
-  if (dropdownContent.style.display === "block") {
-    console.log("dropdownContent.style.display is block ")
-    dropdownContent.style.display = "none";
-  }
-    else  {
-      dropdownContent.style.display= "block";
-    }
-  });
+function fade($ele) {
+  $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+      var $next = $(this).next('.quote');
+      fade($next.length > 0 ? $next : $(this).parent().children().first());
+ });
 }
+fade($('.quoteLoop > .quote').first());
+
+
+/*----------------------------------------------------*/
+/* Navigation
+------------------------------------------------------ */
+
+$(window).scroll(function() {
+
+  if ($(window).scrollTop() > 300) {
+      $('.main_nav').addClass('sticky');
+  } else {
+      $('.main_nav').removeClass('sticky');
+  }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+  if ($('.main_nav').hasClass('open-nav')) {
+      $('.main_nav').removeClass('open-nav');
+  } else {
+      $('.main_nav').addClass('open-nav');
+  }
+});
+
+$('.main_nav li a').click(function() {
+  if ($('.main_nav').hasClass('open-nav')) {
+      $('.navigation').removeClass('open-nav');
+      $('.main_nav').removeClass('open-nav');
+  }
+});
+
+
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+
+jQuery(document).ready(function($) {
+
+ $('.smoothscroll').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+    $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+        window.location.hash = target;
+    });
+});
+
+});
+
+
+TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
